36401d6e605322a12958e1f1aef0040e734671cc
Rashmi <rr456rashmi@gmail.com>
2025-07-15 21:30:19 +0530

    mood ai api backend

---
+++ mood-ai-backend/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+import pickle
+from flask_cors import CORS
+
+# Load model and vectorizer
+model = pickle.load(open("model.pkl", "rb"))
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+
+#Initialize flask app
+app = Flask(__name__)
+
+#cors
+CORS(app)
+
+# Home route
+@app.route("/")
+def home():
+    return "Mood detection API is running!"
+
+# Predict route
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+    text = data.get("text")
+
+    if not text:
+        return jsonify({"error": "No text provided"}), 400
+    
+    vector = vectorizer.transform([text])
+    prediction = model.predict(vector)[0]
+
+    return jsonify({"mood": prediction})
+
+# Run the app
+if __name__== "__main__":
+    app.run(debug=True)
--- mood-ai-backend/mood_trainer.py
+++ mood-ai-backend/mood_trainer.py
@@ -1,4 +1,23 @@
-import pandas;
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import pickle
 
-csvFile = pandas.read_csv("mood_dataset.csv");
-print(csvFile);
+df = pd.read_csv("mood_dataset.csv")
+
+# convert text into numbers
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(df["text"])
+
+# count the numbers
+y = df["mood"]
+model = MultinomialNB()
+model.fit(X, y)
+
+# sample test
+sample = vectorizer.transform(["I feel like crying"])
+print(model.predict(sample))
+
+# save
+pickle.dump(model, open("model.pkl", "wb"))
+pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
